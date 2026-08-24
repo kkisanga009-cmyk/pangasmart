2a373d459307746bdc7c16fb43d64b591081b0a8
Benard Kisanga <kkisanga009@gmail.com>
2026-08-24 15:03:55 +0300

    update

--- src/main/java/com/pangasmart/pangasmart/controllers/PaymentController.java
+++ src/main/java/com/pangasmart/pangasmart/controllers/PaymentController.java
@@ -1,19 +1,21 @@
 package com.pangasmart.pangasmart.controllers;
 
-// 1. REKEBISHA IMPORT HAPA: Tumia models.Payment badala ya entity.Payment
 import com.pangasmart.pangasmart.models.Payment;
 import com.pangasmart.pangasmart.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @Controller
+@RequestMapping("/payments")
 public class PaymentController {
 
     @Autowired
     private PaymentRepository paymentRepository;
 
-    // Hakikisha njia zako zote ndani ya controller hii zina tumia models.Payment
+    @PostMapping("/save")
+    public String savePayment(@ModelAttribute Payment payment) {
+        paymentRepository.save(payment);
+        return "redirect:/dashboard";
+    }
 }
