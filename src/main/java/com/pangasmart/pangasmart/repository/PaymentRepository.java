8121797501d62d12ed85442a47bfa22eae3278a3
Benard Kisanga <kkisanga009@gmail.com>
2026-08-24 14:41:38 +0300

    update

--- src/main/java/com/pangasmart/pangasmart/repository/PaymentRepository.java
+++ src/main/java/com/pangasmart/pangasmart/repository/PaymentRepository.java
@@ -1,6 +1,6 @@
 package com.pangasmart.pangasmart.repository;
 
-import com.pangasmart.pangasmart.models.Payment; // Tumesahihisha import hapa kwenda models
+import com.pangasmart.pangasmart.models.Payment; // Lazima isome .models.Payment na sio .entity.Payment
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
