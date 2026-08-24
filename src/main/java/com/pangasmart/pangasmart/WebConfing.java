02392efda6c0d5015ae2d3d00bdf10927cd270b3
Benard Kisanga <kkisanga009@gmail.com>
2026-08-24 16:42:01 +0300

    update

--- src/main/java/com/pangasmart/pangasmart/WebConfing.java
+++ src/main/java/com/pangasmart/pangasmart/WebConfing.java
@@ -4,12 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfing implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String uploadDir = Paths.get(System.getProperty("user.dir"), "uploads").toAbsolutePath().toUri().toString();
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations(uploadDir);
     }
 }
